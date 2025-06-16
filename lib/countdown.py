027c9556cd51fcfbf090b51288738722a704a2d3
Aisha Hassan <aishahassan669912@gmail.com>
2025-06-16 11:29:41 -0700

    done

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,19 @@
 # your code goes here!
+# lib/countdown.py
+
+def countdown(start=10):
+    while start > 0:
+        print(f"{start} SECOND(S)!")
+        start -= 1
+    print("HAPPY NEW YEAR!")
+    return "HAPPY NEW YEAR!"
+
+
+def countdown_with_sleep(start=10):
+    import time
+    while start > 0:
+        print(f"{start} SECOND(S)!")
+        time.sleep(1)
+        start -= 1
+    print("HAPPY NEW YEAR!")
+    return "HAPPY NEW YEAR!"
